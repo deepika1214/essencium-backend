dd5d0505b4fa25222e49c196a7c2ca408931a771
deepika1214 <deepikav201818@gmail.com>
2026-04-18 14:24:54 +0530

    #865 fix: added missing @Valid to ContactController and SentryProxyController

--- essencium-backend/src/main/java/de/frachtwerk/essencium/backend/controller/ContactController.java
+++ essencium-backend/src/main/java/de/frachtwerk/essencium/backend/controller/ContactController.java
@@ -25,6 +25,7 @@ import de.frachtwerk.essencium.backend.service.ContactMailService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.Set;
@@ -55,6 +56,7 @@ public class ContactController<ID extends Serializable> {
 
   @PostMapping
   @Operation(description = "As a logged in user, send a certain message to the given address")
+  @Valid
   public void sendContactRequest(
       @RequestBody @NotNull final ContactRequestDto contactRequest,
       @Parameter(hidden = true) @AuthenticationPrincipal final EssenciumUserDetails<ID> user) {
